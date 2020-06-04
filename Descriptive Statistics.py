5b231f49b515d4d3b5bdb4ddd4b2d88fff156e45
Edgar Akopyan <47034548+edgarakopyan@users.noreply.github.com>
2020-06-04 15:16:16 +0200

    Update Descriptive Statistics.py

--- Descriptive Statistics.py
+++ Descriptive Statistics.py
@@ -18,7 +18,7 @@ pd.set_option('mode.chained_assignment', None)
 ###################################################################
 # This code requires Data Preparation script.
 # Thus code contains a lot of replication, I will only explain
-# First cases
+# first cases
 
 
 ''' Set the correct work directory '''
@@ -27,9 +27,46 @@ os.chdir('/Users/edgarakopyan/Desktop/Measuring Unfair Inequality/Data')
 
 samplework = pd.read_pickle('Intermediarywork')
 
-ymin = 0
-
 years = list(samplework.year.unique())
+
+# Now we create the poverty thresholds
+rosstat = pd.read_excel("data.xls")
+rosstat = list(rosstat.iloc[4, 1:])
+rosstat = pd.Series(rosstat, index = range(2001, 2019))
+
+ymin = pd.Series( list([145397, 327000, 379000, 425000, 717, 963, 1133]),index = range(1994, 2001))
+ymin = pd.concat([ymin, rosstat])
+
+
+ymin = ymin[~ymin.index.isin([1997, 1999])]
+
+ymin[ymin.index < 1998] = ymin[ymin.index < 1998]/1000
+
+ymin = list(ymin)
+
+# We also need  to adjust for inflation. I use IMF inflation data as opposed to Penn Table because the latter goes only
+# until 2017.
+
+inflation = pd.read_excel('imf-dm-export-20200516.xls')
+
+inflation = inflation.iloc[1, 15:40]  # Select the valid years
+
+inflation = inflation/100 + 1
+# We have to multiply each consequentive element by the previous one to see how much price levels have changed over the
+# period
+for i in range(1, inflation.shape[0]):
+    inflation.iloc[i] = inflation.iloc[i] * inflation.iloc[i-1]
+
+finalinflation = inflation.iloc[inflation.shape[0]-1]
+
+for i in range(0, inflation.shape[0]):
+    inflation.iloc[i] = (inflation.iloc[i])/finalinflation
+
+inflation = inflation.drop([1997, 1999])  # Since we do not have those years anyway
+
+ymin = [x/y for x,y in zip(ymin, inflation)]
+
+ymin = pd.Series(ymin, index= years)
 
 types = list((samplework.type.value_counts()).index)
 
@@ -49,9 +86,9 @@ for j in years:
         if (a[a.type == i].shape[0] < 20) == True:
             nonusedtypes.append(i)
     a = a[~a.type.isin(nonusedtypes)]
-    ymin = a.income.median() * 0.6
-    lowincome = a[a.income <= ymin]
-    HeadcountR.append(a.shape[0] / lowincome.shape[0])
+    YMIN = ymin[j]
+    lowincome = a[a.income <= YMIN]
+    HeadcountR.append((lowincome.shape[0] / a.shape[0]) * 100)
 
 WBheadcount = pd.read_excel("API_SI.POV.NAHC_DS2_en_excel_v2_1071128.xls")
 
@@ -64,9 +101,9 @@ WBheadcount.index = range(1994, 2019)
 
 WBheadcount = WBheadcount.drop(labels = [1997, 1999])
 
-plt.plot(years, HeadcountR, color="Red")
+plt.scatter(years, HeadcountR, color="Red")
 
-plt.plot(years, list(WBheadcount), color="Blue")
+plt.scatter(years, list(WBheadcount), color="Blue")
 
 plt.legend(["Author's Calculations", "World Bank Data"], mode="Expand")
 
@@ -95,16 +132,16 @@ for j in years:
         if (a[a.type == i].shape[0] < 20) == True:
             nonusedtypes.append(i)
     a = a[~a.type.isin(nonusedtypes)]
-    ymin = a.income.median() * 0.6
-    lowincome = a[a.income <= ymin]
-    PovertyGap.append((lowincome.shape[0]/a.shape[0]) - (1 / (a.shape[0] * ymin) ) * lowincome.income.sum())
+    YMIN = ymin[j]
+    lowincome = a[a.income <= YMIN]
+    PovertyGap.append(((lowincome.shape[0]/a.shape[0]) - (1 / (a.shape[0] * YMIN)) * lowincome.income.sum())*100)
 
-WBpoverty = pd.read_excel("API_SI.POV.GAPS_DS2_en_excel_v2_1073352.xls")
+WBpoverty = pd.read_excel("API_SI.POV.UMIC.GP_DS2_en_excel_v2_1126782.xls")
 
 WBpoverty  = WBpoverty [(WBpoverty ['Data Source'] == 'Russian Federation') | (WBpoverty ['Data Source'] == \
                                                                                   'Country Name')]
 
-WBpoverty  = WBpoverty .iloc[1, 38: 63]
+WBpoverty  = WBpoverty.iloc[1, 38: 63]
 
 WBpoverty .index = range(1994, 2019)
 
@@ -115,7 +152,7 @@ plt.plot(years, PovertyGap)
 
 plt.plot(years, WBpoverty)
 
-plt.legend(("Author's Calculations", "World Bank Data (Poverty gap at $1.90 a day 2011 PPP)"))
+plt.legend(("Author's Calculations", "World Bank Data (Poverty gap at $5.50 a day 2011 PPP)"))
 
 plt.title("Poverty Gap in Russia from 1994 to 2018", loc='center')
 
@@ -133,9 +170,9 @@ plt.clf()
 
 Watts = []
 
-def watts(dataframe, ymin):
-    b = dataframe[dataframe.income <= ymin]
-    WattsIndex = (1 / a.shape[0]) * (b.shape[0]) * math.log(ymin) - (1 / a.shape[0]) * \
+def watts(dataframe, YMIN):
+    b = dataframe[dataframe.income <= YMIN]
+    WattsIndex = (1 / a.shape[0]) * (b.shape[0]) * math.log(YMIN) - (1 / a.shape[0]) * \
                 (b.income.transform(math.log)).sum()
     return WattsIndex
 
@@ -148,8 +185,8 @@ for j in years:
         if (a[a.type == i].shape[0] < 20) == True:
             nonusedtypes.append(i)
     a = a[~a.type.isin(nonusedtypes)]
-    ymin = a.income.median() * 0.6
-    Watts.append(watts(a, ymin=ymin))
+    YMIN = ymin[j]
+    Watts.append(watts(a, YMIN = YMIN))
 
 # Now we plot it and save the file
 
@@ -171,6 +208,8 @@ z = Table()
 z['Watts'] = Watts
 
 z['Poverty_Gap'] = PovertyGap
+
+z['ymin'] = ymin
 
 z.write("Intermediarylists", format='csv')
 ###################################################################
@@ -246,4 +285,3 @@ plt.legend(("Author's Calculations", "World Bank Data "))
 plt.savefig("Gini")
 
 plt.clf()
-
